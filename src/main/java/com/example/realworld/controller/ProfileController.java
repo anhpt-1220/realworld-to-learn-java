cbab8be7bd5fb683990ee8d51ab9c732de272db4
anhpt-1220 <pham.the.anh@sun-asterisk.com>
2025-04-14 09:12:42 +0700

    fix comments

--- src/main/java/com/example/realworld/controller/ProfileController.java
+++ src/main/java/com/example/realworld/controller/ProfileController.java
@@ -1,9 +1,11 @@
 package com.example.realworld.controller;
 
 import com.example.realworld.dto.ProfileResDto;
+import com.example.realworld.model.AppUserDetails;
 import com.example.realworld.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,17 +21,20 @@ public class ProfileController {
     private ProfileService profileService;
 
     @GetMapping("/{username}")
-    public ResponseEntity<ProfileResDto> getByUsername(@PathVariable String username) {
-        return ResponseEntity.ok(profileService.getProfile(username));
+    public ResponseEntity<ProfileResDto> getByUsername(@PathVariable String username,
+            @AuthenticationPrincipal AppUserDetails appUserDetails) {
+        return ResponseEntity.ok(profileService.getProfile(username, appUserDetails));
     }
 
     @PostMapping("/{username}/follow")
-    public ProfileResDto followUser(@PathVariable("username") String name) {
-        return profileService.followUser(name);
+    public ProfileResDto followUser(@PathVariable("username") String name,
+            @AuthenticationPrincipal AppUserDetails appUserDetails) {
+        return profileService.followUser(name, appUserDetails);
     }
 
     @DeleteMapping("/{username}/follow")
-    public ProfileResDto unfollowUser(@PathVariable("username") String name) {
-        return profileService.unfollowUser(name);
+    public ProfileResDto unfollowUser(@PathVariable("username") String name,
+            @AuthenticationPrincipal AppUserDetails appUserDetails) {
+        return profileService.unfollowUser(name, appUserDetails);
     }
 }
--- src/main/java/com/example/realworld/service/ProfileService.java
+++ src/main/java/com/example/realworld/service/ProfileService.java
@@ -5,10 +5,10 @@ import com.example.realworld.entity.FollowEntity;
 import com.example.realworld.entity.UserEntity;
 import com.example.realworld.exception.AppException;
 import com.example.realworld.exception.Error;
+import com.example.realworld.model.AppUserDetails;
 import com.example.realworld.repository.FollowRepository;
 import com.example.realworld.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -21,16 +21,11 @@ public class ProfileService {
     private FollowRepository followRepository;
 
 
-    public ProfileResDto getProfile(String name) {
+    public ProfileResDto getProfile(String name, AppUserDetails appUserDetails) {
         UserEntity user = userRepository.findByUsername(name).orElseThrow(() -> new AppException(
                 Error.USER_NOT_FOUND));
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        UserEntity currentUserEntity = userRepository.findByEmail(email)
-                .orElseThrow(() -> new AppException(
-                        Error.USER_NOT_FOUND
-                ));
         boolean following = followRepository.findByFollowingIdAndFollowedById(user.getId(),
-                currentUserEntity.getId()).isPresent();
+                appUserDetails.getUserEntity().getId()).isPresent();
         return ProfileResDto.builder()
                 .username(user.getUsername())
                 .image(user.getImage())
@@ -39,21 +34,17 @@ public class ProfileService {
                 .build();
     }
 
-    public ProfileResDto followUser(String name) {
+    public ProfileResDto followUser(String name, AppUserDetails appUserDetails) {
         UserEntity followingUser = userRepository.findByUsername(name)
                 .orElseThrow(() -> new AppException(
                         Error.USER_NOT_FOUND));
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        UserEntity followedBy = userRepository.findByEmail(email)
-                .orElseThrow(() -> new AppException(
-                        Error.USER_NOT_FOUND
-                ));
-        followRepository.findByFollowingIdAndFollowedById(followingUser.getId(), followedBy.getId())
+        followRepository.findByFollowingIdAndFollowedById(followingUser.getId(),
+                        appUserDetails.getUserEntity().getId())
                 .ifPresent(follow -> {
                     throw new AppException(Error.ALREADY_FOLLOWED_USER);
                 });
 
-        followRepository.save(new FollowEntity(followingUser, followedBy));
+        followRepository.save(new FollowEntity(followingUser, appUserDetails.getUserEntity()));
         return ProfileResDto.builder()
                 .username(followingUser.getUsername())
                 .image(followingUser.getImage())
@@ -62,17 +53,13 @@ public class ProfileService {
                 .build();
     }
 
-    public ProfileResDto unfollowUser(String name) {
+    public ProfileResDto unfollowUser(String name, AppUserDetails appUserDetails) {
         UserEntity followingUser = userRepository.findByUsername(name)
                 .orElseThrow(() -> new AppException(
                         Error.USER_NOT_FOUND));
-        String email = SecurityContextHolder.getContext().getAuthentication().getName();
-        UserEntity followerUser = userRepository.findByEmail(email)
-                .orElseThrow(() -> new AppException(
-                        Error.USER_NOT_FOUND
-                ));
-        FollowEntity follow = followRepository.findByFollowingIdAndFollowedById(followingUser.getId(),
-                        followerUser.getId())
+        FollowEntity follow = followRepository.findByFollowingIdAndFollowedById(
+                        followingUser.getId(),
+                        appUserDetails.getUserEntity().getId())
                 .orElseThrow(() -> new AppException(Error.FOLLOW_NOT_FOUND));
         followRepository.delete(follow);
         return ProfileResDto.builder()
